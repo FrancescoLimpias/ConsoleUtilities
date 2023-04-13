cf556566fe4eff58baac3b2b0f06630377e83c16
Francesco Limpias <francescolimpias@gmail.com>
2023-04-13 16:44:08 +0200

    Add project files.

---
+++ FancyConsole/FancyConsole.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FancyConsole
+{
+    public class FancyConsole
+    { 
+
+        //True if there is an active FancyConsole instance
+        private static bool Instanced { get; set; } = false;
+
+        //Fancy Console PROPERTIES
+        //window title
+        private string WindowTitle {
+            get
+            {
+                return Console.Title;
+            } 
+            set
+            {
+                Console.Title = value;
+            }
+        }
+        //FancyConsole size
+        private int width, height;
+        
+        public FancyConsole(string windowTitle, int width, int height)
+        {
+            //Check whether there already is an instanced FancyConsole
+            if (Instanced)
+            {
+                throw new Exception("Only one FancyConsole instance allowed at a time");
+            }
+
+            //Toggle instanced
+            Instanced = true;
+
+            //Set properties
+            WindowTitle = windowTitle;
+            this.width = width;
+            this.height = height;
+
+        }
+        ~FancyConsole()
+        {
+            //Toggle back instanced
+            Instanced = false;
+        }
+
+    }
+}
---
+++ FancyConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FancyConsole
+{
+    internal class Program
+    {
+
+        static FancyConsole FC;
+        static void Main(string[] args)
+        {
+            //Instantiate FancyConsole
+            FC = new FancyConsole("Boolean - Exercise", 75, 20);
+
+            FC.Header("Exercise 1");
+            FC.SubHeader("by Francesco Limpias");
+
+            /*
+            FC.Paragraph("Questo è un test 2 + 2 = " + 2 + 2);
+
+            FC.Options(
+                    new Option(Key.R, () => ),
+                    new Option(Key.Q, () => ),
+                    FancyConsole.ExitOption
+                );
+            */
+        }
+    }
+}
